b3c4dd79f7e086f64964c1ab0d741c29ca3c3516
Francisco Abarza <francisco@abar.za.com>
2016-10-16 23:33:54 -0300

    first commit basic structure app

---
+++ app/src/main/java/com/story/interactive/abarza/interactivestory/MainActivity.java
@@ -0,0 +1,46 @@
+package com.story.interactive.abarza.interactivestory;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+  private EditText mNameField;
+  private Button mStartButton;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    bindUi();
+
+    mStartButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String name = mNameField.getText().toString();
+        Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+      }
+    });
+
+  }
+
+  public void bindUi() {
+    mNameField = (EditText) findViewById(R.id.nameEditText);
+    mStartButton = (Button) findViewById(R.id.startButton);
+  }
+
+
+}
